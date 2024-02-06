a08b69cfb0b44f27b3e7cf72f66aecec97a13fc3
JinseoKim <eunoia@handong.ac.kr>
2024-02-06 14:53:53 +0900

    lazy import

--- src/Router/Router.js
+++ src/Router/Router.js
@@ -1,4 +1,4 @@
-import React, { Suspense } from "react";
+import React, { Suspense, lazy } from "react";
 import { Route, Routes } from "react-router-dom";
 
 const HomePage = lazy(() => import("../Page/Home"));
